9bc717347c389bedbb25a66ada9b75e8000bc204
torco34 <torcoromaarias@gmail.com>
2024-10-08 08:39:38 -0500

    Primer commit curso

---
+++ statements/01_select.sql
@@ -0,0 +1,6 @@
+SELECT * FROM users ;
+-- consulta base de datos de la tabla
+SELECT name FROM users ;
+-- trae los nombre de la base de datos de la tabla
+SELECT user_id, name FROM users ;
+-- nombre del usuario y el id de la base de datos de la tabla
---
+++ statements/03_where.sql
@@ -0,0 +1,12 @@
+SELECT * FROM users WHERE age = 15
+-- solo quiero que me traiga la edad 15
+
+SELECT  name FROM users WHERE age = 15
+
+-- 
+
+SELECT DISTINCT name FROM users WHERE age = 15
+
+-- 
+
+SELECT DISTINCT age FROM users WHERE age = 15
